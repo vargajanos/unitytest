1057f59f7b351d587a006bac0b065cd982d70038
vargajanos <janosvarga706@gmail.com>
2023-05-25 10:12:29 +0200

    teszt

---
+++ Assets/Course Library/Obstacles/scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(30, 0, -10);
+    public GameObject player; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
